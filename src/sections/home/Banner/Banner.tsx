0961c691572a0bf6c1e772a6dd6a9a06fe0e16db
Deckstar <dextersibirtsev_ise@yahoo.com>
2020-10-11 12:57:33 +0300

    Added scrolling to Banner.tsx buttons; refactored Banner.tsx to map items;

--- src/sections/home/Banner/Banner.tsx
+++ src/sections/home/Banner/Banner.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
 import { FaGithub, FaGoodreads, FaTwitter } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
+import { scroller } from 'react-scroll';
+import { IconType } from 'react-icons';
 import { Box, Button, Container, Link, Typography } from '@material-ui/core';
+import { map } from 'lodash';
 import { photos } from '@images';
 import useStyles from './Banner.style';
+
+interface SectionButtonProps {
+  title: string;
+  link: string;
+}
+
+interface SocialLinkProps {
+  link: string;
+  Icon: IconType;
+}
+
+const sectionButtons: SectionButtonProps[] = [
+  {
+    title: 'Home',
+    link: 'banner',
+  },
+  {
+    title: 'About/CV',
+    link: 'about',
+  },
+  {
+    title: 'Skills',
+    link: 'skills',
+  },
+  {
+    title: 'Experience',
+    link: 'experience',
+  },
+  {
+    title: 'Contact',
+    link: 'contact',
+  },
+];
+
+const socialLinks: SocialLinkProps[] = [
+  {
+    link: 'https://twitter.com/DexterSibirtsev',
+    Icon: FaTwitter,
+  },
+  {
+    link: 'https://github.com/Deckstar',
+    Icon: FaGithub,
+  },
+  {
+    link: 'https://www.goodreads.com/user/show/58196314-dexter',
+    Icon: FaGoodreads,
+  },
+];
+
+const handleScrollTo = (id: string) => {
+  scroller.scrollTo(id, {
+    offset: -50, // so navbar doesn't block view
+    duration: 600,
+    smooth: true,
+  });
+};
 
 const Banner = () => {
   const { t } = useTranslation();
@@ -22,42 +81,39 @@ const Banner = () => {
               {t('Banner.SoftwareDeveloper')}
             </Typography>
             <Typography variant="h5">
-              Hello! I am a professional programmer, specializing in websites &
-              mobile apps.
+              Hello! I am a professional programmer, specializing in websites
+              &amp; mobile apps.
             </Typography>
           </Box>
 
           <Box className={classes.icons}>
-            <Link
-              href="https://twitter.com/DexterSibirtsev"
-              className={classes.icon}
-            >
-              <FaTwitter size={26} />
-            </Link>
-            <Link href="https://github.com/Deckstar" className={classes.icon}>
-              <FaGithub size={26} />
-            </Link>
-            <Link
-              href="https://www.goodreads.com/user/show/58196314-dexter"
-              className={classes.icon}
-            >
-              <FaGoodreads size={26} />
-            </Link>
+            {map(socialLinks, (item, i) => {
+              const { link, Icon } = item;
+              return (
+                <Link
+                  className={classes.icon}
+                  href={link}
+                  key={`social link ${i}`}
+                >
+                  <Icon size={26} />
+                </Link>
+              );
+            })}
           </Box>
 
           <Box className={classes.buttons}>
-            <Link href="#about" className="button">
-              <Button variant="contained">About Me &amp; CV</Button>
-            </Link>
-            <Link href="#skills" className="button">
-              <Button variant="contained">Skills</Button>
-            </Link>
-            <Link href="#education" className="button">
-              <Button variant="contained">Education</Button>
-            </Link>
-            <Link href="#contact" className="button">
-              <Button variant="contained">Contact</Button>
-            </Link>
+            {map(sectionButtons, (button, i) => {
+              const { title, link } = button;
+              return (
+                <Button
+                  variant="contained"
+                  onClick={() => handleScrollTo(link)}
+                  key={`banner button ${i}`}
+                >
+                  {title}
+                </Button>
+              );
+            })}
           </Box>
         </Container>
       </Box>
