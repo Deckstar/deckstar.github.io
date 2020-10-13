f97675b50226e9667a2c3e220a2832e8f96a37c1
Deckstar <dextersibirtsev_ise@yahoo.com>
2020-10-13 22:41:16 +0300

    Fixed dark mode imports;

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
 import { IconButton } from '@material-ui/core';
 import {
-  Brightness4 as LightIcon,
-  Brightness7 as DarkIcon,
+  Brightness4 as DarkIcon,
+  Brightness7 as LightIcon,
 } from '@material-ui/icons';
 import { Context } from '@context';
 
 const DarkModeIcon = (props: { darkMode: boolean }) => {
   const { darkMode } = props;
 
-  if (darkMode) return <LightIcon />;
-  return <DarkIcon />;
+  // if dark mode is on, show a moon
+  if (darkMode) return <DarkIcon />;
+  return <LightIcon />;
 };
 
 const DarkModeToggle = () => {
