b43a7391418247b907c2a18fde514b155339001f
Deckstar <dextersibirtsev_ise@yahoo.com>
2020-11-02 19:29:02 +0200

    Cleaned up some commented code;

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -1,7 +1,6 @@
 import React, { memo } from 'react';
 import { Box, Card, CardContent, CardHeader } from '@material-ui/core';
-import { map, replace, size } from 'lodash';
-import { LineHighImage } from '@components';
+import { map, size } from 'lodash';
 import { SkillCardProps, SkillItem } from '@typescript/@types/skills';
 import useStyles from './SkillCard.style';
 import GatsbyImage from 'gatsby-image';
@@ -42,19 +41,10 @@ const Extra = (props: { extra: SkillItem['extra'] }) => {
 const Logo = (props: { src: SkillItem['src'] }) => {
   const { src } = props;
   const classes = useStyles();
-  // console.log('stuff', src?.childImageSharp.fluid);
 
   if (src) {
-    // const widthPx = 20;
     const widthPx: number = src?.childImageSharp.fluid.presentationWidth;
     const heightPx: number = src?.childImageSharp.fluid.presentationHeight;
-    // const pxString: string = src?.childImageSharp.fluid.sizes.match(
-    //   / \d+px/
-    // )[0];
-    // const widthPx = Number(replace(replace(pxString, ' ', ''), 'px', ''));
-
-    // console.log('pxString', pxString);
-    console.log('widthPx', widthPx);
 
     return (
       <GatsbyImage
