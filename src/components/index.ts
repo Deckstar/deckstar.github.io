09e683d4a04d644c4d85f10a68f7569f35fa7713
Deckstar <dextersibirtsev_ise@yahoo.com>
2022-01-26 23:53:35 +0200

    Internal - deleted unused `LoadingScreen` component;

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { default as ExperienceItem } from './ExperienceItem/ExperienceItem';
 export { default as HideOnScroll } from './HideOnScroll/HideOnScroll';
 export { default as Layout } from './Layout/Layout';
 export { default as LineHighImage } from './LineHighImage/LineHighImage';
-export { default as LoadingScreen } from './LoadingScreen/LoadingScreen';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as SEO } from './SEO/SEO';
 export { default as SkillCard } from './SkillCard/SkillCard';
