f74e19301c0ce04b37c03e9393669a91e0de1a8d
Deckstar <dextersibirtsev_ise@yahoo.com>
2022-01-27 22:19:55 +0200

    Improve - refacted `LanguageMenu` flags to use `GatsbyImage`;

--- src/components/Navbar/LanguageMenu.tsx
+++ src/components/Navbar/LanguageMenu.tsx
@@ -6,18 +6,29 @@ import {
   SelectProps,
 } from '@mui/material';
 import { langMap, LanguageCode, LanguageObject } from '@utils/languages';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 import { useI18next } from 'gatsby-plugin-react-i18next';
-import { find, map } from 'lodash';
-import React, { useCallback } from 'react';
+import { map, mapKeys } from 'lodash';
+import React, { useCallback, useMemo } from 'react';
 
 import useStyles from './Navbar.style';
 
-const Flag = (props: { code: LanguageCode; flag: any } & IconButtonProps) => {
+interface FlagProps extends IconButtonProps {
+  code: LanguageCode;
+  flag: FlagNode;
+}
+
+const Flag = (props: FlagProps) => {
   const { code, flag, ...iconButtonProps } = props;
 
   return (
     <IconButton {...iconButtonProps} color="inherit" size="large">
-      <img src={flag?.default} alt={`${code} flag`} style={{ height: 24 }} />
+      <GatsbyImage
+        image={flag?.src?.childImageSharp?.gatsbyImageData}
+        alt={`${code} flag`}
+        style={{ width: 24, height: 24 }}
+      />
     </IconButton>
   );
 };
@@ -32,24 +43,81 @@ const FlagChoice = (props: LanguageObject) => {
   );
 };
 
-const SelectedFlag = (value: any) => {
+type FlagsMap = {
+  [K in LanguageCode]: FlagNode;
+};
+
+interface SelectedFlagProps extends Omit<FlagProps, 'flag'> {
+  flags: FlagsMap;
+}
+
+const SelectedFlag = (props: SelectedFlagProps) => {
+  const { code: value, flags } = props;
   if (!value) {
     return null;
   }
 
-  const currentLang = find(langMap, ({ code }) => code === value) || langMap[0];
+  const flag = flags[value];
 
-  const { flag, code } = currentLang;
-  return <Flag code={code} flag={flag} />;
+  return <Flag code={value} flag={flag} />;
 };
 
 const LanguageInputComponent = () => {
   return null;
 };
 
+type FlagNode = {
+  code: LanguageCode;
+  name: string;
+  src: {
+    name: string;
+    childImageSharp: {
+      gatsbyImageData: IGatsbyImageData;
+    };
+  };
+};
+
+type FlagsResponse = {
+  allFlagsJson: {
+    nodes: FlagNode[];
+  };
+};
+
 const LanguageMenu = () => {
   const { classes } = useStyles();
   const { changeLanguage, language } = useI18next();
+
+  const flagsData = useStaticQuery<FlagsResponse>(graphql`
+    {
+      allFlagsJson {
+        nodes {
+          code
+          name
+          src {
+            name
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED, height: 40)
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const flags = useMemo(() => {
+    const flagNodes = flagsData?.allFlagsJson?.nodes;
+
+    const keyedFlags = mapKeys(
+      flagNodes,
+      (flagNode) => flagNode?.code
+    ) as FlagsMap;
+    return keyedFlags;
+  }, [flagsData]);
+
+  const renderSelectedFlag = useCallback(
+    (value: LanguageCode) => <SelectedFlag code={value} flags={flags} />,
+    [flags]
+  );
 
   const handleChangeLanguage = useCallback<
     NonNullable<SelectProps<LanguageCode>['onChange']>
@@ -72,14 +140,16 @@ const LanguageMenu = () => {
       IconComponent={LanguageInputComponent}
       inputProps={{ name: 'language' }}
       classes={{ select: classes.languageSelect }}
-      renderValue={SelectedFlag}
+      renderValue={renderSelectedFlag}
       variant="standard"
     >
       {map(langMap, (lang) => {
         const { code } = lang || {};
+        const flag = flags[code];
+
         return (
           <MenuItem key={code} value={code}>
-            <FlagChoice {...lang} />
+            <FlagChoice {...lang} flag={flag} />
           </MenuItem>
         );
       })}
