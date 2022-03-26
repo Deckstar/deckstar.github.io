bab8b82a15f0e2a6aceb777b9d09fa82fbc59874
Deckstar <dextersibirtsev_ise@yahoo.com>
2022-03-26 10:45:19 +0200

    Updates

---
+++ component---src-pages-404-tsx-9b29cd7648cc66249790.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[218],{9786:function(t,e,l){l.r(e);var n=l(5444),r=l(7294);e.default=function(){return(0,r.useEffect)((function(){(0,n.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-9b29cd7648cc66249790.js.map
